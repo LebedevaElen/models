413d7323a7cb5557ba220e9278bbacc5f31aa961
Elena Lebedeva <llenka96@yandex.ru>
2018-11-09 11:31:42 +0700

    Train/eval split

--- xml_to_csv.py
+++ xml_to_csv.py
@@ -4,28 +4,26 @@ import sys
 import pandas as pd
 import xml.etree.ElementTree as ET
 
-
-def xml_to_csv(path):
+path = sys.argv[1]
+os.chdir(path)
+for set_type in ['train', 'eval']:
     xml_list = []
-    for xml_file in glob.glob(path + '/*.xml'):
-        tree = ET.parse(xml_file)
-        root = tree.getroot()
-        for member in root.findall('object'):
-            value = (root.find('filename').text,
-                     int(root.find('size')[0].text),
-                     int(root.find('size')[1].text),
-                     member[0].text,
-                     int(member[4][0].text),
-                     int(member[4][1].text),
-                     int(member[4][2].text),
-                     int(member[4][3].text)
-                     )
-            xml_list.append(value)
+    with open(f'{set_type}.txt') as f:
+        for xml_file in f:
+            tree = ET.parse(f'xmls/{xml_file}.xml')
+            root = tree.getroot()
+            for member in root.findall('object'):
+                value = (root.find('filename').text,
+                         int(root.find('size')[0].text),
+                         int(root.find('size')[1].text),
+                         member[0].text,
+                         int(member[4][0].text),
+                         int(member[4][1].text),
+                         int(member[4][2].text),
+                         int(member[4][3].text)
+                         )
+                xml_list.append(value)
     column_name = ['filename', 'width', 'height', 'class', 'xmin', 'ymin', 'xmax', 'ymax']
     xml_df = pd.DataFrame(xml_list, columns=column_name)
-    return xml_df
-
-annotations_path = sys.argv[1]
-xml_df = xml_to_csv(annotations_path)
-xml_df.to_csv('labels.csv', index=None)
-print('Successfully converted xml to csv.')
+    xml_df.to_csv(f'{set_type}_labels.csv', index=None)
+    print('Successfully converted xml to csv.')
