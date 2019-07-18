b92843c407d3c478cfa640fb94a11c9d443871b1
Elena Lebedeva <llenka96@yandex.ru>
2019-07-18 19:17:08 +0700

    Update generate_tfrecord.py

--- generate_tfrecord.py
+++ generate_tfrecord.py
@@ -29,12 +29,6 @@ FLAGS = flags.FLAGS
 def class_text_to_int(row_label):
     if row_label == 'person':
         return 1
-    elif row_label == 'asurg':
-        return 2
-    elif row_label == 'asurg_box':
-        return 3
-    else:
-        None
 
 
 def split(df, group):
